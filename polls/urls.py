809041ba77539419caecd5b6b07886651bf6c3a7
kubkowski <jakub.opara@gmail.com>
2015-02-26 10:11:46 +0100

    Finished part 4 of the official tutorial

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, patterns
 
-from . import views
+from polls import views
 
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
-    url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+urlpatterns = patterns('',
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
-]
+)
